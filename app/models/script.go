d73ac44e0620500f7d5e890bb25343fce38d6ecb
Brian Seitel <brian@rentjuice.com>
2015-03-22 22:46:17 -0700

    Remove old dead code

--- app/models/script.go
+++ app/models/script.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"log"
+
 )
 
 type Script struct {
@@ -10,18 +10,6 @@ type Script struct {
     Interval string `db:"interval"`
     LastCheckin string `db:"last_checkin"`
     Severity int64
-}
-
-func FindScript(id int) (bool, *Script) {
-	obj, err := dbmap.Get(Script{}, id)
-	script := obj.(*Script)
-
-	if err != nil {
-		log.Print("ERROR findScript: ")
-		log.Println(err)
-	}
-
-	return (err == nil), script
 }
 
 func (s *Script) Late() bool {
